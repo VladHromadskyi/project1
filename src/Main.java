2ffa9d7806e710e32d50f07b05dcdbab32b9f4d0
Vlad Hromadskyi <s32732@pjwstk.edu.pl>
2026-03-18 20:25:16 +0100

    Replaced part of the code in main into a new method "distance" in Observation class

--- src/Main.java
+++ src/Main.java
@@ -10,16 +10,6 @@ public class Main {
             throw new RuntimeException(e);
         }
         list.show();
-//        Scanner scanner = new Scanner(System.in);
-//        double[] vector1 = readVector(scanner,4);
-//        double[] vector2 = readVector(scanner,4);
-//        double distance = 0;
-//        for(int i =0;i<4;i++){
-//            distance+=Math.pow(vector2[i]-vector1[i],2);
-//        }
-//        distance=Math.sqrt(distance);
-//        System.out.println("Distance = "+ distance);
-
     }
     private static double[] readVector(Scanner sc, int n){
         double[] vector = new double[n];
--- src/Observation.java
+++ src/Observation.java
@@ -19,4 +19,16 @@ public class Observation {
         }
         System.out.println(flowerName+"\n");
     }
+    public double distance(Observation obj){
+        if(this.attributes.length!=obj.attributes.length){
+            throw new IllegalArgumentException("Vectors have different length: "+this.attributes.length
+            +" and " + obj.attributes.length);
+        }
+        double sum = 0;
+        for(int i =0;i<this.attributes.length;i++){
+            double diff = obj.attributes[i]-this.attributes[i];
+            sum+=diff*diff;
+        }
+        return Math.sqrt(sum);
+    }
 }
