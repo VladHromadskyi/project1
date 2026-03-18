ded11ce154e3e25c4453a3fb0202bbebfb1a7870
Vlad Hromadskyi <s32732@pjwstk.edu.pl>
2026-03-18 19:16:32 +0100

    Add Neighbor class with basic logic

---
+++ src/Neighbor.java
@@ -0,0 +1,16 @@
+public class Neighbor implements Comparable<Neighbor>{
+    private Observation observation;
+    private double distance;
+
+    public Neighbor(Observation observation, double distance){
+        this.observation = observation;
+        this.distance = distance;
+    }
+    @Override
+    public int compareTo(Neighbor o) {
+        return Double.compare(this.distance,o.distance);
+    }
+
+    public Observation getObservation(){return this.observation;}
+    public double getDistance(){return this.distance;}
+}
